9511730ec9996811bed07455aa4c9a45d219c202
abhishek.goyal2 <abhi25goel@gmail.com>
2018-05-16 14:23:46 +0530

    commit change

---
+++ src/GLAssingment1/PracticeExercise1.java
@@ -0,0 +1,47 @@
+package GLAssingment1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PracticeExercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.setProperty("webdriver.chrome.driver", "C:\\Projects\\chromedriver_win32 (1)\\chromedriver.exe");
+
+//Opening the chrome driver and 		
+WebDriver driver =new ChromeDriver();
+		driver.get("http://store.demoqa.com/");
+		
+		//Getting page title and title lenght
+		String Pagetitle=driver.getTitle();
+		System.out.println(Pagetitle);
+		
+		int Pagetitlesize=Pagetitle.length();
+		System.out.println(Pagetitlesize);
+		
+		
+		//Get current url and check if it is expected or not
+		String curenturl=driver.getCurrentUrl();
+		String expurl="http://store.demoqa.com/products-page/product-category/";
+//		String expurl="http://store.demoqa.com/";
+		if(curenturl.equals(expurl))
+		{
+			System.out.println("currenturl is expected url" + curenturl);
+		}
+		else
+		{
+			System.out.println("currenturl is not matching expected url" );
+		}
+		
+		//Get page source and printlength
+		String Pagesource =driver.getPageSource();
+		System.out.println(Pagesource);
+		int Pagsourcelen=Pagesource.length();
+		System.out.println(Pagsourcelen);
+		
+		driver.close();
+
+	}
+
+}
---
+++ src/GLAssingment1/PracticeExercise2.java
@@ -0,0 +1,31 @@
+package GLAssingment1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PracticeExercise2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+System.setProperty("webdriver.chrome.driver", "C:\\Projects\\chromedriver_win32 (1)\\chromedriver.exe");
+
+//Opening the chrome driver and 		
+WebDriver driver =new ChromeDriver();
+		driver.get("http://demoqa.com/frames-and-windows/");
+		
+		driver.findElement(By.xpath(".//*[@id='ui-id-1']")).click();
+		Thread.sleep(500);
+		driver.findElement(By.xpath(".//*[@id='tabs-1']/div/p/a")).click();
+		Thread.sleep(800);
+		
+//		driver.findElement(By.xpath(".//*[@id='ui-id-2']")).click();
+//		Thread.sleep(500);
+//		driver.findElement(By.xpath(".//*[@id='tabs-2']/div/p/a")).click();
+//		Thread.sleep(800);
+		
+		driver.quit();
+
+	}
+
+}
